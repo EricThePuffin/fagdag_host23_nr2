d808e8c68fbff9d7005b8e4b962f3ee81fc61816
EricThePuffin <143482638+EricThePuffin@users.noreply.github.com>
2023-11-15 08:53:44 +0100

    Siste push før levering ig

--- Data eksempeleksamen/Eksempeleksamen22 uten KI.py
+++ Data eksempeleksamen/Eksempeleksamen22 uten KI.py
@@ -1,14 +1,24 @@
 import pandas as pd
-
+previous_started_at = None
 startsteder = {}
+d = 0
+weekdays = [0, 0, 0, 0, 0, 0, 0]
 with open("Data eksempeleksamen/Eksempeleksamen22.csv", "r", encoding="ISO-8859-1") as file:
     data_frame = pd.read_csv(file)
-    for start_station_name in data_frame['start_station_name']:
+    for start_station_name in data_frame["start_station_name"]:
         if start_station_name in startsteder:
             startsteder[start_station_name] += 1
         else:
             startsteder[start_station_name] = 1
-
+    for started_at in data_frame["started_at"]:
+        weekdays[d] += 1
+        if started_at != previous_started_at:
+            if d < 6:
+                d += 1
+            else:
+                d = 0
+        previous_started_at = started_at
+print(weekdays)
 mest_vanlige_startsteder = dict(sorted(startsteder.items(), key=lambda item: item[1], reverse=True))
 for i, k in enumerate(mest_vanlige_startsteder.keys()):
     if i == 3:
@@ -16,7 +26,7 @@ for i, k in enumerate(mest_vanlige_startsteder.keys()):
     print(k, mest_vanlige_startsteder[k])
 
 mest_uvanlige_startsteder = dict(sorted(startsteder.items(), key=lambda item: item[1]))
-for i, k in enumerate(mest_vanlige_startsteder.keys()):
+for i, k in enumerate(mest_uvanlige_startsteder.keys()):
     if i == 3:
         break
     print(k, mest_uvanlige_startsteder[k])
--- Data næringer/omsetning.py
+++ Data næringer/omsetning.py
@@ -1,9 +1,10 @@
+"""
 import pandas as pd
 
 data = []
 header = []
 
-with open("Data næringer/omsetning.csv", "r", encoding="ISO-8859-1") as file:
+with open() as file:
     data_frame = pd.read_csv(file)
     i = 0
     for row in data_frame:
@@ -16,4 +17,27 @@ with open("Data næringer/omsetning.csv", "r", encoding="ISO-8859-1") as file:
             data.append(row)
             print(row)
         i = 1
+print(data)
+"""
+import csv
+
+title = []
+header = []
+data = []
+
+with open("Data næringer/omsetning.csv", "r", encoding="ISO-8859-1", newline='\r\n') as file:
+    reader = csv.reader(file, delimiter = ";")
+    i = 0
+    for row in file:
+        if i == 0:
+            title.append(row)
+        elif i == 1:
+            header.append(row)
+        elif not (row):
+            continue
+        else:
+            data.append(row)
+        i += 1
+print(title)
+print(header)
 print(data)
